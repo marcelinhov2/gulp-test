1c72a95aa41b65f77d1ef704c547a5ac278ed9ca
Marcelo Eduardo de Andrade Jr <marceloandrade150@gmail.com>
2016-02-04 17:08:03 -0200

    remove trash

--- www/scripts/controllers/aboutController.js
+++
@@ -1,4 +0,0 @@
-angular.module('aboutController', [])
-.controller('aboutCtrl', function($scope){
-	$scope.message = 'state about';
-});
--- www/scripts/controllers/contactController.js
+++
@@ -1,4 +0,0 @@
-angular.module('contactController', [])
-.controller('contactCtrl', function($scope){
-	$scope.message = 'state contact';
-});
